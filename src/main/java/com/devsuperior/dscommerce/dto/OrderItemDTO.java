b89893439fc7b666a1545dd533d7798772882b24
Vitor-PereiraS <vitorpereirasou2010@gmail.com>
2026-02-17 15:01:30 -0300

    Saving Order

--- src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
@@ -1,16 +1,15 @@
 package com.devsuperior.dscommerce.dto;
 
-import com.devsuperior.dscommerce.entities.Order;
 import com.devsuperior.dscommerce.entities.OrderItem;
 
 public class OrderItemDTO {
-    private Long id;
+    private Long productId;
     private String name;
     private Double price;
     private Integer quantity;
 
     public OrderItemDTO(Long id, String name, Double price, Integer quantity) {
-        this.id = id;
+        this.productId = id;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
@@ -18,15 +17,15 @@ public class OrderItemDTO {
     }
 
     public OrderItemDTO(OrderItem entity) {
-        id = entity.getProduct().getId();
+        productId = entity.getProduct().getId();
         name = entity.getProduct().getName();
         price = entity.getPrice();
         quantity = entity.getQuantity();
 
     }
 
-    public Long getId() {
-        return id;
+    public Long getProductId() {
+        return productId;
     }
 
     public String getName() {
