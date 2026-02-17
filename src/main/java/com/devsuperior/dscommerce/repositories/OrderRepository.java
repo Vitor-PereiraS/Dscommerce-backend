e6927f5d9823d285309a7dd29e8502a4a4e9c2cf
Vitor-PereiraS <vitorpereirasou2010@gmail.com>
2026-02-17 13:11:47 -0300

    OrderDTO, get order by id

---
+++ src/main/java/com/devsuperior/dscommerce/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dscommerce.repositories;
+
+import com.devsuperior.dscommerce.entities.Order;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
